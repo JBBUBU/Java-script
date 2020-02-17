462f787f7b04cd1e65f3995372e57f3aa215a640
BUBU <jeanbaptbuniet@gmail.com>
2020-02-17 12:08:27 +0100

    TEST

---
+++ TP Condition/Scripts/Java S Condition.js
@@ -0,0 +1,23 @@
+var nom = prompt("Donnez un nom");
+
+switch (nom) {
+	case 'Sarah':
+		alert('Sarah Connor ?');
+		break;
+
+	case 'Siri':
+		alert('Je n ai pas compris votre question');
+		break;
+
+	case 'Vandam':
+		alert('Rentre chez toi');
+		break;
+
+    case 'Harry':
+		alert('Retourne a lecole');
+		break;
+
+	default:
+		alert('oh! cest un joli prénom');
+		break;
+	}
